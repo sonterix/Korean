2baeaa8f5e0400ff5af529914c2e755076b6b462
Nick <sonterixo@gmail.com>
2018-06-16 01:23:55 +0300

    added sweetalert

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -10,25 +10,47 @@ $(function() {
 
     $('.x-btn i').on('click', function() {
 
-        const conf = confirm('Are you sure?');
-        if(conf === false) {
-            return false;
-        }
+        // const conf = confirm('Are you sure?');
+        // if(conf === false) {
+        //     return false;
+        // }
 
-        const tr = $(this).parent().parent();
-        const doramaId = tr.find('.dorama-id').text();
+        swal({
+            title: 'Вы уверены?',
+            text: "Дорама будет удалена!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#952347',
+            cancelButtonColor: '#212529',
+            confirmButtonText: 'Да, удалить',
+            cancelButtonText: 'Отмена'
+        }).then((result) => {
+            if (result.value) {
+                const tr = $(this).parent().parent();
+                const doramaId = tr.find('.dorama-id').text();
+        
+                $.ajax({
+                    url: '/deleteDorama',
+                    type: 'post',
+                    dataType: 'json',
+                    data: {
+                       element: doramaId
+                    },
+                    complete: function() {
+                        tr.remove();
 
-        $.ajax({
-            url: '/deleteDorama',
-            type: 'post',
-            dataType: 'json',
-            data: {
-               element: doramaId
-            },
-            complete: function() {
-                tr.remove();
-            },
-         });
+                        swal({
+                            title: 'Удалено!',
+                            text: 'Дорама успешно удалена',
+                            type: 'success',
+                            confirmButtonColor: '#212529',
+                        })
+                    },
+                 })
+            } else {
+                return false;
+            }
+        })
     })
 
     $('.edit-btn i').on('click', function() {
